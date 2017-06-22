3720df9ea190435ebeb3f7033eecc18320add737
Nikita Seleckis <nikita@seleckis.lv>
2017-06-22 10:35:50 +0300

    dummies moved to another path

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,8 +5,8 @@ import {ProductItem} from './containers/ProductItem/ProductItem.jsx';
 
 const prodProps = {
     category: 'optional category',
-    name: 'product name',
-    img: '/dist/metering-product-dummy-01.jpg',
+    title: 'product name',
+    img: '/../_temp/products/metering-product-dummy-01.jpg',
     description: 'some text about the product item',
     items: [
         {
