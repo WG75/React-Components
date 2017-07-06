f3d26893fe878ff18f7f14529ed8b207d1627600
Nikita Seleckis <nikita@seleckis.lv>
2017-07-06 22:28:13 +0300

    use nested options

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,72 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Selectbox from './containers/Selectbox/c/Selectbox';
-import CSelectBoxes from './containers/Selectbox/CSelectBoxes.jsx'
-// import options from './data/options.js'
+import CSelectBoxes from './containers/Selectbox/CSelectBoxes.jsx';
+import options from './data/options.js';
 
-const options = [
-    [
-        {
-            value: 'value1',
-            label: 'value2'
-        },
-        {
-            value: 'value1',
-            label: 'value2'
-        },
-        {
-            value: 'value1',
-            label: 'value2'
-        }
-
-    ],
-    [
-        {
-            value: 'value2',
-            label: 'value2'
-        },
-        {
-            value: 'value1',
-            label: 'value2'
-        },
-        {
-            value: 'value1',
-            label: 'value2'
-        }
-
-
-    ],
-    [
-        {
-            value: 'value3',
-            label: 'value3'
-        },
-        {
-            value: 'value1',
-            label: 'value2'
-        },
-        {
-            value: 'value1',
-            label: 'value2'
-        }
-
-
-    ],
-    [
-        {
-            value: 'value4',
-            label: 'value4'
-        },
-        {
-            value: 'value1',
-            label: 'value2'
-        },
-        {
-            value: 'value1',
-            label: 'value2'
-        }
-
-    ]
-]
+// const options = [
+//     [
+//         {
+//             value: 'value1',
+//             label: 'value2'
+//         },
+//         {
+//             value: 'value1',
+//             label: 'value2'
+//         },
+//         {
+//             value: 'value1',
+//             label: 'value2'
+//         }
+//
+//     ],
+//     [
+//         {
+//             value: 'value2',
+//             label: 'value2'
+//         },
+//         {
+//             value: 'value1',
+//             label: 'value2'
+//         },
+//         {
+//             value: 'value1',
+//             label: 'value2'
+//         }
+//
+//
+//     ],
+//     [
+//         {
+//             value: 'value3',
+//             label: 'value3'
+//         },
+//         {
+//             value: 'value1',
+//             label: 'value2'
+//         },
+//         {
+//             value: 'value1',
+//             label: 'value2'
+//         }
+//
+//
+//     ],
+//     [
+//         {
+//             value: 'value4',
+//             label: 'value4'
+//         },
+//         {
+//             value: 'value1',
+//             label: 'value2'
+//         },
+//         {
+//             value: 'value1',
+//             label: 'value2'
+//         }
+//
+//     ]
+// ]
 
 ReactDOM.render((<CSelectBoxes SBListOfOptions={options}/>), document.getElementById('root'));
